928a65c52447d042ed323a9b38f7c264ff871f1c
Lary <lari.lobaina@gmail.com>
2025-05-02 21:03:18 -0400

    feat: add checkouttest

---
+++ SauceDemo/Pages/CarPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SauceDemo.Config;
+
+namespace SauceDemo.Pages
+{
+    public class CarPage(IWebDriver driver) : BasePage(driver)
+    {
+        private readonly IWebDriver _driver = driver;
+
+        private IWebElement CheckOut => _driver.WaitForElementToBeClickable(By.Id("checkout"));
+    
+        public void ClickCheckOut() {
+                CheckOut.ClickElement();
+            }
+
+        }
+}
+
+
